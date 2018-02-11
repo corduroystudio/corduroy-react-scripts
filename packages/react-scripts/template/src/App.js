372f2b01b1d1973187c2d4140ffe1465958781b3
owain-stratton <owain@geoshepherds.com>
2018-02-11 21:25:04 +0100

    added prettier rc file for default

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Component } from "react";
+import styles from "./App.scss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Corduroy Studio - React App Starter</h1>
+      <div className={styles.app}>
+        <header className={styles["app-header"]}>
+          <h1 className={styles["app-title"]}>
+            Corduroy Studio - React App Starter
+          </h1>
         </header>
-        <p className="App-intro">
+        <p className={styles["app-intro"]}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
